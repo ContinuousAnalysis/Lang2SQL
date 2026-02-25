d88fca454b23792e3a1b3dd1de7b0b7973a661a7
seyeong <seyoung4503@gmail.com>
2026-02-25 17:32:06 +0900

    chore: export new public API symbols in __init__.py

--- src/lang2sql/__init__.py
+++ src/lang2sql/__init__.py
@@ -1,20 +1,33 @@
 from .components.execution.sql_executor import SQLExecutor
 from .components.generation.sql_generator import SQLGenerator
+from .components.retrieval.chunker import CatalogChunker, DocumentChunkerPort, RecursiveCharacterChunker
+from .components.retrieval.index_builder import IndexBuilder
 from .components.retrieval.keyword import KeywordRetriever
-from .core.catalog import CatalogEntry
+from .components.retrieval.vector import VectorRetriever
+from .core.catalog import CatalogEntry, IndexedChunk, RetrievalResult, TextDocument
 from .core.exceptions import ComponentError, IntegrationMissingError, Lang2SQLError
 from .core.hooks import MemoryHook, NullHook, TraceHook
-from .core.ports import DBPort, LLMPort
+from .core.ports import DBPort, EmbeddingPort, LLMPort, VectorStorePort
 from .flows.nl2sql import BaselineNL2SQL
 
 __all__ = [
     # Data types
     "CatalogEntry",
+    "TextDocument",
+    "IndexedChunk",
+    "RetrievalResult",
     # Ports (protocols)
     "LLMPort",
     "DBPort",
+    "EmbeddingPort",
+    "VectorStorePort",
     # Components
     "KeywordRetriever",
+    "VectorRetriever",
+    "IndexBuilder",
+    "DocumentChunkerPort",
+    "CatalogChunker",
+    "RecursiveCharacterChunker",
     "SQLGenerator",
     "SQLExecutor",
     # Flows
